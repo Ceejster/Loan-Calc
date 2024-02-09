92921f317a316e5af9228c5ac233e9fbda479a31
Ceej <CJBowman@live.com>
2024-02-09 13:37:14 -0600

    Set up JS functions and all layout
    Figure out the table set up for HTML and which values need to go where.
    JS functions are laid out put need to be cleaned up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-function HelloWorld() {
-    alert("Hello World");
+function getValues() {
+    let loanAmount = document.getElementById("userLoan").value;
+    let monthAmount = document.getElementById("userMonths").value;
+    let rateAmount = document.getElementById("userRate").value;
+    let monthOrYear = document.querySelector('input[name="monthYear"]:checked').value;
+
+    let interest = interestCalculator(loanAmount, rateAmount);
+    let dollar = moneyFormat(interest);
+
+    document.getElementById("principalAmount").innerHTML = `${dollar}`;
+
+}
+
+function moneyFormat(format) {
+    let money = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(format)
+    return money;
+}
+
+function interestCalculator(loan, rate) {
+    let decimalRate = rate / 100;
+    let monthlyRate = decimalRate / 12;
+    let monthlyInterest = monthlyRate * loan;
+    let montlhyPercent = monthlyRate * 100;
+
+    return monthlyInterest;
 }
